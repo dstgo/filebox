818a66f7fa3d0897ba118d2614474019c0034c8e
246859 <2633565580@qq.com>
2023-07-23 15:11:07 +0800

    feat(copy): finished copy

---
+++ copy.go
@@ -0,0 +1,107 @@
+package filebox
+
+import (
+	"io"
+	"io/fs"
+	"os"
+	"path"
+)
+
+type RwFile interface {
+	fs.File
+	io.ReadWriteCloser
+}
+
+type RwFs interface {
+	ReadFs
+	WriteFs
+}
+
+type ReadFs interface {
+	Open(name string) (fs.File, error)
+	ReadDir(name string) ([]fs.DirEntry, error)
+	ReadFile(name string) ([]byte, error)
+}
+
+type WriteFs interface {
+	Mkdir(name string) error
+	MkdirAll(name string) error
+	WriteFile(name string, data []byte) error
+	CreateFile(name string) (RwFile, error)
+}
+
+var Os OsFs
+
+type OsFs struct {
+}
+
+func (s *OsFs) CreateFile(name string) (RwFile, error) {
+	return os.OpenFile(name, os.O_RDWR|os.O_CREATE, 0666)
+}
+
+func (s *OsFs) Mkdir(name string) error {
+	return os.Mkdir(name, os.ModeDir)
+}
+
+func (s *OsFs) MkdirAll(name string) error {
+	return os.MkdirAll(name, os.ModeDir)
+}
+
+func (s *OsFs) WriteFile(name string, data []byte) error {
+	return os.WriteFile(name, data, os.ModePerm)
+}
+
+func (s *OsFs) ReadDir(name string) ([]fs.DirEntry, error) {
+	return os.ReadDir(name)
+}
+
+func (s *OsFs) ReadFile(name string) ([]byte, error) {
+	return os.ReadFile(name)
+}
+
+func (s *OsFs) Open(name string) (fs.File, error) {
+	return os.Open(name)
+}
+
+func CopyDir(srcFS ReadFs, dstFs WriteFs, src, dst string) error {
+	dir, err := srcFS.ReadDir(src)
+	if err != nil {
+		return err
+	} else if err = dstFs.MkdirAll(dst); err != nil {
+		return err
+	}
+
+	for _, entry := range dir {
+		dstPath := path.Join(dst, entry.Name())
+		srcPath := path.Join(src, entry.Name())
+		var copyErr error
+		if entry.IsDir() {
+			copyErr = CopyDir(srcFS, dstFs, srcPath, dstPath)
+		} else {
+			copyErr = CopyFile(srcFS, dstFs, srcPath, dstPath)
+		}
+
+		if copyErr != nil {
+			return copyErr
+		}
+	}
+	return nil
+}
+
+func CopyFile(srcFs ReadFs, dstFs WriteFs, src, dst string) error {
+	srcFile, err := srcFs.Open(src)
+	if err != nil {
+		return err
+	}
+	dstFile, err := dstFs.CreateFile(dst)
+	if err != nil {
+		return err
+	}
+
+	_, err = io.Copy(dstFile, srcFile)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/dstgo/filebox
+
+go 1.20
