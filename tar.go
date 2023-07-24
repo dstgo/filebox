5355da8fd7b1a35823ae663aca8adb5912bbf2d0
246859 <2633565580@qq.com>
2023-07-24 21:34:38 +0800

    fix(zip): 修复了 zip压缩的目录结构问题

--- tar.go
+++ tar.go
@@ -1,11 +1,26 @@
 package filebox
 
+import (
+	"archive/tar"
+	"compress/gzip"
+)
+
 // Tar
 // param src string
 // param dest string
 // return error
 // 以tar格式压缩文件
 func Tar(src, dest string) error {
+	tarFile, err := CreateFile(dest)
+	if err != nil {
+		return err
+	}
+	gzWriter := gzip.NewWriter(tarFile)
+	defer gzWriter.Close()
+
+	tarWriter := tar.NewWriter(gzWriter)
+	defer tarWriter.Close()
+
 	return nil
 }
 
